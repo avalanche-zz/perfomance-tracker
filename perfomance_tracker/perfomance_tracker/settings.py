98104cf18e9129219cb20e1bb926bdf67dc32bf8
Andrey Nosov <63540265+avalanche-zz@users.noreply.github.com>
2022-05-18 09:40:55 +0700

    Work on templates and manage files

--- perfomance_tracker/perfomance_tracker/settings.py
+++ perfomance_tracker/perfomance_tracker/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'perfomance_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
